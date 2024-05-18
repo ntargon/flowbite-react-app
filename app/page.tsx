dfe7951600506abfeb5c03a11c987202b2667ac3
ntargon <argon19951128@gmail.com>
2024-05-18 14:51:00 +0900

    axios sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+"use client";
+
 import { DarkThemeToggle } from "flowbite-react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [post, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setPosts(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+    <main>
       <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
       <DarkThemeToggle />
+      <div>
+        {post.map((val) => {
+          return <div key={val.id}>{val.id}</div>;
+        })}
+      </div>
     </main>
   );
 }
